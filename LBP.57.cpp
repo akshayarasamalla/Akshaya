10866eea183707eb1a4f5d706bbdee2050128096
akshayarasamalla <akshayarasamalla08@gmail.com>
2025-04-17 15:45:42 +0530

    Add files via upload
    LBP 53,54,57,58

---
+++ LBP.57.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,r,count=0;
+	scanf("%d",&n);
+	while(n>0)
+	{
+		r=n%10;
+		n=n/10;
+		if(r%2==0)
+		{
+			count=count+1;
+		}
+	}
+	printf("%d",count);
+	return 1;
+}
